27c966cb01584334bcc0f1cc6d2fc8d888c8eda6
Nihat <abdullazadenihat@gmail.com>
2022-12-05 02:44:01 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,52 @@
+var btn = document.querySelector('button');
+btn.addEventListener('click', btnClick);
+function btnClick() {
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+    var k = 30; // px
+    var _loop_1 = function (i) {
+        var circle = document.createElement('div');
+        var square = document.createElement('div');
+        var triangle = document.createElement('div');
+        circle.classList.add('circle');
+        square.classList.add('square');
+        triangle.classList.add('triangle');
+        circle.style.top = height / 2 + i * k + "px";
+        circle.style.left = width / 2 + "px";
+        square.style.top = height / 2 + "px";
+        square.style.left = width / 2 + i * k + "px";
+        triangle.style.top = height / 2 + "px";
+        triangle.style.left = width / 2 - i * k + "px";
+        document.body.append(circle, square, triangle);
+        var circleTimer = setInterval(function () {
+            if (parseInt(circle.style.top) < 200) {
+                circle.remove();
+                clearInterval(circleTimer);
+            }
+            else {
+                circle.style.top = parseInt(circle.style.top) - 2 + 'px';
+            }
+        }, 5);
+        var squareTimer = setInterval(function () {
+            if (parseInt(square.style.left) > 1300) {
+                square.remove();
+                clearInterval(squareTimer);
+            }
+            else {
+                square.style.left = parseInt(square.style.left) + 2 + 'px';
+            }
+        }, 5);
+        var triangleTimer = setInterval(function () {
+            if (parseInt(triangle.style.left) < 200) {
+                triangle.remove();
+                clearInterval(triangleTimer);
+            }
+            else {
+                triangle.style.left = parseInt(triangle.style.left) - 2 + 'px';
+            }
+        }, 5);
+    };
+    for (var i = 1; i <= 20; i += 1) {
+        _loop_1(i);
+    }
+}
